79312c0530a1e004c0d5421bb63f7e3426a1bb86
Yair <yair.janbar@gmail.com>
2024-11-07 12:14:06 +0200

    test1

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,8 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
+	    // String name1 = args[0];
 	    // Replace this comment with the rest of your code   
+		System.out.println("hello world");
 	}
 }
