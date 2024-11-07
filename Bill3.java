2de8c1f48a89614f501f9146336ffba64cfdd2fa
Yair <yair.janbar@gmail.com>
2024-11-07 18:45:31 +0200

    try this

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,9 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    // String name1 = args[0];
+		int dinnerBill = 300;
+		System.out.println("each one of you need to pay" + dinnerBill/3);
+
 	    // Replace this comment with the rest of your code   
 		System.out.println("hello world");
 	}
