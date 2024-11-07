dce1b541c9d4b39a7e8e265cf6b5f199e350ebcb
Yair <yair.janbar@gmail.com>
2024-11-07 20:45:49 +0200

    try five

--- Bill3.java
+++ Bill3.java
@@ -2,11 +2,14 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    // String name1 = args[0];
-		int dinnerBill = 300;
-		System.out.println("each one of you need to pay" + dinnerBill/3);
-
+	    String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+		String dinnerBill = args[3];
+		int finalBill = Integer.parseInt(dinnerBill);
+		double divided = Math.ceil(finalBill/3);
+		System.out.println("Hello "+ name1 + ", " + name2 + " and " + name3 + ". " + "each one of you need to pay" + divided);
 	    // Replace this comment with the rest of your code   
-		System.out.println("hello world");
-	}
+		// System.out.println("hello world");
+    }
 }
