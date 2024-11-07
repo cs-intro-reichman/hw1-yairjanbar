ea6e3a160c3065936ee56c0dc113f3dc9a6a9e14
Yair <yair.janbar@gmail.com>
2024-11-07 21:40:18 +0200

    maybe it will work now

--- Bill3.java
+++ Bill3.java
@@ -8,7 +8,7 @@ public class Bill3 {
 		String dinnerBill = args[3];
 		int finalBill = Integer.parseInt(dinnerBill);
 		double divided = Math.ceil(finalBill/3.0);
-		System.out.println("Dear "+ name3 + ", " + name2 + " and " + name1 + ": " + "pay " + divided+ " shekels each ");
+		System.out.println("Dear "+ name3 + ", " + name2 + " and " + name1 + ": " + "pay " + divided+ " Shekels each ");
 	    // Replace this comment with the rest of your code   
 		// System.out.println("hello world");
     }
