f4a082bbc41f9127dcb8df1ead9b9986d8b80ff4
Yair <yair.janbar@gmail.com>
2024-11-08 00:38:08 +0200

    first try

--- FVCalc.java
+++ FVCalc.java
@@ -2,5 +2,13 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
+		int money = Integer.parseInt(args[0]);
+		double percent = Double.parseDouble(args[1]);
+		int years = Integer.parseInt(args[2]);
+		int firstPow = (int)(Math.pow(((percent / 100) + 1.0), years));
+		int futureValue = money*firstPow;
+		System.out.println("After " + years + " years, a $" + money + " saved at " + percent + "% will yield $" + futureValue);
+		//int finalValue = (int)money*(Math.pow(((percent/100.0)+1),years));
+
 	}
 }
